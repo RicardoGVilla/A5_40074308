fb538f2e663a8833050b544c708246901f59819b
Ricardo Villanueva <ricardovillanueva@MacBook-Air-2.local>
2025-08-05 16:13:24 -0400

    Task 1

---
+++ assignment.cpp
@@ -0,0 +1,43 @@
+#include "assignment5.h"
+#include <iterator>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm> 
+#include <string>  
+
+
+
+// Task 1
+/* 
+    Write a function that opens a file (one word per line) and copies all words into an empty WordVec using std::copy
+        - Open a file 
+            - use std::ifstream 
+            - handle file open errors by throwing std::runtime_error
+            - use the istream iterator to define the begin and end 
+        - extract the one word per file 
+        - copy each word per line into a WordVec using ::copy 
+            std::copy(begin, end, std::back inserter(words vector));.
+            - return the words_vector 
+*/
+
+WordVec read_words_into_vector(const std::string& file_name) { 
+    // function implementation     
+    std::ifstream file(file_name); 
+
+    if(!file) { 
+         throw std::runtime_error("Failed to open the file.");
+    }
+
+    WordVec words_vector; 
+
+    std::istream_iterator<std::string> begin(file); 
+    std::istream_iterator<std::string> end; 
+
+    // copy from stream into vector
+    std::copy(begin, end, std::back_inserter(words_vector));
+
+    return words_vector; 
+
+
+}
+
---
+++ assignment5.h
@@ -0,0 +1,12 @@
+
+#ifndef ASSIGNMENT5_H
+#define ASSIGNMENT5_H
+
+
+// Task 1
+WordVec read_words_into_vector(const std::string& file_name);
+
+
+
+
+#endif // ASSIGNMENT5_H
