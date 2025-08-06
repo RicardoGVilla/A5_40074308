468785c3286206c6f6e336fd76427ba4e73e6643
Ricardo Villanueva <ricardovillanueva@MacBook-Air-2.local>
2025-08-06 12:58:45 -0400

    Task 2 and Task 3

--- assignment.cpp
+++ assignment.cpp
@@ -41,3 +41,44 @@ WordVec read_words_into_vector(const std::string& file_name) {
 
 }
 
+
+// Task 2
+/* 
+   Counting word occurrences and inserting them into a map using for each lambda 
+     -  iterate over using each 
+     - using a lambda function to add the instance to the map object 
+*/
+WordMap map_and_count_words_using_lambda(const WordVec& wVec) {
+    WordMap wMap;
+    std::for_each(wVec.begin(), wVec.end(), 
+                  [&wMap](const std::string& word) { ++wMap[word]; });
+    return wMap;
+}
+
+
+// Task 3
+
+/* 
+    Counting word occurrences and inserting them into a map using for each functor 
+     -  iterate over using each 
+     - using a functor function to add the instance to the map object 
+        - create a class to call the functor 
+        - use the same class to keep track using a member state to keep track of the number of instances
+        - return the map 
+*/
+
+class WordCountFunctor {
+private:
+    WordMap wMap;
+public:
+    void operator()(const std::string& word) {
+        ++wMap[word];
+    }
+    WordMap getMap() const { return wMap; }
+};
+
+WordMap map_and_count_words_using_functor(const WordVec& wVec) {
+    WordCountFunctor wcf;
+    wcf = std::for_each(wVec.begin(), wVec.end(), wcf);
+    return wcf.getMap();
+}
--- assignment5.h
+++ assignment5.h
@@ -3,10 +3,22 @@
 #define ASSIGNMENT5_H
 
 
-// Task 1
+// Type aliases
+using WordVec = std::vector<std::string>;
+using WordList = std::list<std::string>;
+using WordMap = std::map<std::string, size_t>;
+
+
+
+// Task 1 Using std::copy to Extract Words from File
 WordVec read_words_into_vector(const std::string& file_name);
 
+// Task 2: Counting Word Occurrences with Lambda
+WordMap map_and_count_words_using_lambda(const WordVec& wVec);
 
+
+// Task 3: Counting Word Occurrences with Functor
+WordMap map_and_count_words_using_functor(const WordVec& wVec);
 
 
 #endif // ASSIGNMENT5_H
